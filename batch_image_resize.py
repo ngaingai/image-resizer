f260bddd85d28f05c4255a6ec1f50a907504a102
Al'x <alex.ngai@gmail.com>
2020-06-30 16:52:44 +0900

    Copy directory tree and append _resized

--- batch_image_resize.py
+++ batch_image_resize.py
@@ -1,35 +1,48 @@
 import os
+import shutil
 from PIL import Image
 
-# Check if script is running directly
 if __name__ == "__main__":
-    output_dir = 'resized'
     dir = os.getcwd()
-    input_dir = 'images'
-    full_input_dir = dir + '/' + input_dir
+    resized_dir = dir + "_resized"
 
-    # If output directory doesn't exist, create it
-    if not os.path.exists(os.path.join(dir, output_dir)):
-        os.mkdir(output_dir)
+    #TODO: Check if resized directory exits, if not then prompt to overwrite it
+    #if os.path.exists(resized_dir):
+    #   overwrite()
 
-    try:
-        for file in os.listdir(full_input_dir):     # Iterate through files and resize them
-            resize_image(input_dir, file, output_dir)
-    except OSError: # Error message if input directory doesn't exist
-        print('file not found')
+    shutil.copytree(dir, resized_dir)   # Copies entire source directory
+    
 
-    def resize_image(input_dir, infile, output_dir="resized", size=(320, 180)):
-        outfile = os.path.splitext(infile)[0] + "_resized"      # Append _resized to the file name
-        extension = os.path.splitext(infile)[1]                 # Get file extension
+
+# Check if script is running directly
+# if __name__ == "__main__":
+#     output_dir = 'resized'
+#     dir = os.getcwd()
+#     input_dir = 'images'
+#     full_input_dir = dir + '/' + input_dir
+
+#     # If output directory doesn't exist, create it
+#     if not os.path.exists(os.path.join(dir, output_dir)):
+#         os.mkdir(output_dir)
+
+#     try:
+#         for file in os.listdir(full_input_dir):     # Iterate through files and resize them
+#             resize_image(input_dir, file, output_dir)
+#     except OSError: # Error message if input directory doesn't exist
+#         print('file not found')
+
+#     def resize_image(input_dir, infile, output_dir="resized", size=(320, 180)):
+#         outfile = os.path.splitext(infile)[0] + "_resized"      # Append _resized to the file name
+#         extension = os.path.splitext(infile)[1]                 # Get file extension
         
-        try:
-            img = Image.open(input_dir + '/' + infile)              # Pillow 'Image' class opens file
-            img = img.resize((size[0], size[1]), Image.LANCZOS)     # Resize the image and resample with LANCZOS
+#         try:
+#             img = Image.open(input_dir + '/' + infile)              # Pillow 'Image' class opens file
+#             img = img.resize((size[0], size[1]), Image.LANCZOS)     # Resize the image and resample with LANCZOS
 
-            new_file = output_dir + "/" + outfile + extension
-            img.save(new_file)
-        except IOError:
-            print("unable to resize image {}".format(infile))
+#             new_file = output_dir + "/" + outfile + extension
+#             img.save(new_file)
+#         except IOError:
+#             print("unable to resize image {}".format(infile))
         
         
-        pass
+#         pass
